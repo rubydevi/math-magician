d09fce93e41830b70ebcfc6c1cab00f5050a8716
rubydevi <ariesabyss@gmail.com>
2023-07-11 14:48:55 +0530

    Modify Calculator to use the child component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,37 +1,38 @@
 import React from 'react';
+import Button from './Button';
 import './Calculator.css';
 
 const Calculator = () => (
   <div id="calculator">
     <div className="display">0</div>
     <div className="buttons">
-      <button type="button" className="button">AC</button>
-      <button type="button" className="button">+/-</button>
-      <button type="button" className="button">%</button>
-      <button type="button" className="button right-button">/</button>
+      <Button type="button" className="button">AC</Button>
+      <Button type="button" className="button">+/-</Button>
+      <Button type="button" className="button">%</Button>
+      <Button type="button" className="button right-button">/</Button>
     </div>
     <div className="buttons">
-      <button type="button" className="button">7</button>
-      <button type="button" className="button">8</button>
-      <button type="button" className="button">9</button>
-      <button type="button" className="button right-button">x</button>
+      <Button type="button" className="button">7</Button>
+      <Button type="button" className="button">8</Button>
+      <Button type="button" className="button">9</Button>
+      <Button type="button" className="button right-button">x</Button>
     </div>
     <div className="buttons">
-      <button type="button" className="button">4</button>
-      <button type="button" className="button">5</button>
-      <button type="button" className="button">6</button>
-      <button type="button" className="button right-button">-</button>
+      <Button type="button" className="button">4</Button>
+      <Button type="button" className="button">5</Button>
+      <Button type="button" className="button">6</Button>
+      <Button type="button" className="button right-button">-</Button>
     </div>
     <div className="buttons">
-      <button type="button" className="button">1</button>
-      <button type="button" className="button">2</button>
-      <button type="button" className="button">3</button>
-      <button type="button" className="button right-button">+</button>
+      <Button type="button" className="button">1</Button>
+      <Button type="button" className="button">2</Button>
+      <Button type="button" className="button">3</Button>
+      <Button type="button" className="button right-button">+</Button>
     </div>
     <div className="buttons">
-      <button type="button" className="button zero-button">0</button>
-      <button type="button" className="button">.</button>
-      <button type="button" className="button right-button">=</button>
+      <Button type="button" className="button zero-button">0</Button>
+      <Button type="button" className="button">.</Button>
+      <Button type="button" className="button right-button">=</Button>
     </div>
   </div>
 );
