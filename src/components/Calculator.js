9e026754058cfa90465d0773e5abefc98022b423
rubydevi <ariesabyss@gmail.com>
2023-07-12 17:49:33 +0530

    Make calculator operational

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import './Calculator.css';
-import '../logic/calculate';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div id="calculator">
-    <div className="display">0</div>
-    <div className="buttons">
-      <Button type="button" className="button">AC</Button>
-      <Button type="button" className="button">+/-</Button>
-      <Button type="button" className="button">%</Button>
-      <Button type="button" className="button right-button">/</Button>
+const Calculator = () => {
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonName) => {
+    setCalculatorData((prevState) => calculate(prevState, buttonName));
+  };
+
+  const displayValue = calculatorData.next || calculatorData.total || '0';
+
+  return (
+    <div id="calculator">
+      <div className="display">{displayValue}</div>
+      <div className="buttons">
+        <Button buttonName="AC" className="button" onClick={() => handleButtonClick('AC')} />
+        <Button buttonName="+/-" className="button" onClick={() => handleButtonClick('+/-')} />
+        <Button buttonName="%" className="button" onClick={() => handleButtonClick('%')} />
+        <Button buttonName="÷" className="button right-button" onClick={() => handleButtonClick('÷')} />
+      </div>
+
+      <div className="buttons">
+        <Button buttonName="7" className="button" onClick={() => handleButtonClick('7')} />
+        <Button buttonName="8" className="button" onClick={() => handleButtonClick('8')} />
+        <Button buttonName="9" className="button" onClick={() => handleButtonClick('9')} />
+        <Button buttonName="x" className="button right-button" onClick={() => handleButtonClick('x')} />
+      </div>
+      <div className="buttons">
+        <Button buttonName="4" className="button" onClick={() => handleButtonClick('4')} />
+        <Button buttonName="5" className="button" onClick={() => handleButtonClick('5')} />
+        <Button buttonName="6" className="button" onClick={() => handleButtonClick('6')} />
+        <Button buttonName="-" className="button right-button" onClick={() => handleButtonClick('-')} />
+      </div>
+      <div className="buttons">
+        <Button buttonName="1" className="button" onClick={() => handleButtonClick('1')} />
+        <Button buttonName="2" className="button" onClick={() => handleButtonClick('2')} />
+        <Button buttonName="3" className="button" onClick={() => handleButtonClick('3')} />
+        <Button buttonName="+" className="button right-button" onClick={() => handleButtonClick('+')} />
+      </div>
+      <div className="buttons">
+        <Button buttonName="0" className="button zero-button" onClick={() => handleButtonClick('0')} />
+        <Button buttonName="." className="button" onClick={() => handleButtonClick('.')} />
+        <Button buttonName="=" className="button right-button" onClick={() => handleButtonClick('=')} />
+      </div>
     </div>
-    <div className="buttons">
-      <Button type="button" className="button">7</Button>
-      <Button type="button" className="button">8</Button>
-      <Button type="button" className="button">9</Button>
-      <Button type="button" className="button right-button">x</Button>
-    </div>
-    <div className="buttons">
-      <Button type="button" className="button">4</Button>
-      <Button type="button" className="button">5</Button>
-      <Button type="button" className="button">6</Button>
-      <Button type="button" className="button right-button">-</Button>
-    </div>
-    <div className="buttons">
-      <Button type="button" className="button">1</Button>
-      <Button type="button" className="button">2</Button>
-      <Button type="button" className="button">3</Button>
-      <Button type="button" className="button right-button">+</Button>
-    </div>
-    <div className="buttons">
-      <Button type="button" className="button zero-button">0</Button>
-      <Button type="button" className="button">.</Button>
-      <Button type="button" className="button right-button">=</Button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
