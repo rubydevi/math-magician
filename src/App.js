b3ce24262314c3ff5e4dbb28e8939a11921c6e01
rubydevi <ariesabyss@gmail.com>
2023-07-18 18:36:48 +0530

    Add Navbar and Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import {
+  BrowserRouter, Route, Routes, Link,
+} from 'react-router-dom';
 import React from 'react';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 
 const App = () => (
-  <div className="mainApp">
-    <Calculator />
-    <Quote />
-  </div>
+  <BrowserRouter>
+    <nav>
+      <ul className="nav-list">
+        <li>
+          <Link className="nav-item" to="/">Home</Link>
+        </li>
+        <li>
+          <Link className="nav-item" to="/calculator">Calculator</Link>
+        </li>
+        <li>
+          <Link className="nav-item" to="/quote">Quote</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
