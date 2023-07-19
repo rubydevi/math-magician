b5fc2ab584a983c37bd40464332bca0e5007aa12
rubydevi <ariesabyss@gmail.com>
2023-07-19 12:30:34 +0530

    Add website description in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,15 @@
 const Home = () => (
-  <h1>You are home</h1>
+  <div id="home" className="section-bg-image">
+    <h1>Welcome to our page!</h1>
+    <div className="home-body">
+      <p>
+        The Happy Calculator is where numbers meet laughter! Crunch numbers, solve problems,
+        and giggle along the way with our calculator. Need a quick break? Hop over to our
+        Funny Quotes page & discover smiles and giggles galore! Let&apos;s calculate and
+        laugh together! 😄🔢🎉
+      </p>
+    </div>
+  </div>
 );
 
 export default Home;
