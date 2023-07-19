47c1586593a8fee3a1efcebe6b2c0e1b082f31f5
rubydevi <ariesabyss@gmail.com>
2023-07-19 17:37:54 +0530

    Import Navbar component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import {
-  BrowserRouter, Route, Routes, Link,
+  BrowserRouter, Route, Routes,
 } from 'react-router-dom';
 import React from 'react';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
-import logo from './images/logo1.png';
+import Navbar from './components/Navbar';
 
 const App = () => (
   <BrowserRouter>
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li className="logo-item">
-          <Link className="logo-link" to="/">
-            <img className="logo" src={logo} alt="Logo" />
-            <span className="website-name">Happy Calculator</span>
-          </Link>
-        </li>
-        <li>
-          <Link className="nav-item" to="/">Home</Link>
-        </li>
-        <li>
-          <Link className="nav-item" to="/calculator">Calculator</Link>
-        </li>
-        <li>
-          <Link className="nav-item" to="/quote">Quote</Link>
-        </li>
-      </ul>
-    </nav>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/calculator" element={<Calculator />} />
